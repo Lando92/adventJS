3668b8ac3b9cd66e14a2282bcf3bfcde718c0a08
Orlando <osierra@level6.co>
2021-12-05 09:41:48 -0500

    Implementing Challenge #3 Grinch-Christmas

---
+++ index.js
@@ -0,0 +1,26 @@
+const goodFit = 'bici coche (balón) bici coche peluche';
+const badFit = 'peluche (bici [coche) bici coche balón';
+console.log(isValid(badFit));
+
+export default function isValid(letter) {
+  let arr = letter.split('');
+  let openParentesis = [];
+  let closedParentesis = [];
+  let openFlag = false;
+  let valid = true;
+  arr.forEach((x) => {
+    if ((x === '{' || x === '[') && openFlag) {
+      valid = false;
+    }
+    if (x === '(') {
+      openParentesis.push(x);
+      openFlag = true;
+    }
+    if (x === ')' && openFlag) {
+      closedParentesis.push(x);
+      openFlag = false;
+    }
+  });
+
+  return valid && openParentesis.length === closedParentesis.length;
+}
