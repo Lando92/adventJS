443c9980d28bc7a1f12d2d3d596bf4a84cc96938
Orlando <osierra@level6.co>
2021-12-05 09:59:48 -0500

    Implementing Challenge #2 ListGifts

--- index.js
+++ index.js
@@ -2,25 +2,16 @@ const goodFit = 'bici coche (balón) bici coche peluche';
 const badFit = 'peluche (bici [coche) bici coche balón';
 console.log(isValid(badFit));
 
-export default function isValid(letter) {
-  let arr = letter.split('');
-  let openParentesis = [];
-  let closedParentesis = [];
-  let openFlag = false;
-  let valid = true;
-  arr.forEach((x) => {
-    if ((x === '{' || x === '[') && openFlag) {
-      valid = false;
-    }
-    if (x === '(') {
-      openParentesis.push(x);
-      openFlag = true;
-    }
-    if (x === ')' && openFlag) {
-      closedParentesis.push(x);
-      openFlag = false;
-    }
-  });
-
-  return valid && openParentesis.length === closedParentesis.length;
+export default function listGifts(letter) {
+  return letter
+    .split(' ')
+    .filter((x) => x.length && !x.startsWith('_'))
+    .reduce((acum, curr) => {
+      if (acum.hasOwnProperty(curr)) {
+        acum[curr] = acum[curr] + 1;
+      } else {
+        acum[curr] = 1;
+      }
+      return acum;
+    }, {});
 }
